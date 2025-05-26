009ffb923813302dc1d4d4759b7b0d4077b1a47b
Ge Hagen <ge@enlightensystems.co.nz>
2025-05-26 18:32:30 +1200

    Update for NDIv6 for Windows

--- ndi/build.rs
+++ ndi/build.rs
@@ -38,7 +38,7 @@ fn win_link_and_load() {
     std::fs::copy(src, dst).unwrap();
 }
 
-fn lin_link_and_load() {
+fn linux_link_and_load() {
     println!("cargo:rustc-link-lib=ndi",);
     let mut lib_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     lib_path.push("thirdparty/Linux/Lib");
@@ -55,11 +55,26 @@ fn lin_link_and_load() {
     std::fs::copy(src, dst).unwrap();
 }
 
+fn macos_link_and_load() {
+    println!("cargo:rustc-link-lib=ndi",);
+    let mut lib_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    lib_path.push("thirdparty/Macos/Lib");
+    println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
+
+    // copy dll to OUT_DIR
+    let out_path = get_output_path();
+    let src = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("thirdparty/Macos/Lib/libndi.4.dylib");
+    let dst = Path::join(&out_path, "libndi.4.dylib");
+    std::fs::copy(src, dst).unwrap();
+}
+
 fn main() {
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     match os.as_str() {
         "windows" => win_link_and_load(),
-        "linux" => lin_link_and_load(),
+        "linux" => linux_link_and_load(),
+        "macos" => macos_link_and_load(),
         _ => panic!("Unsupported OS for NDI"),
     };
 }

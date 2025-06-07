4ae8998f37370710c2445ad3b71022400be7b13a
Ge Hagen <ge@enlightensystems.co.nz>
2025-06-07 13:05:34 +1200

    remove cargo fmt action

--- ndi-examples/src/find.rs
+++ ndi-examples/src/find.rs
@@ -12,7 +12,9 @@ fn main() {
     println!("Discovered Sources:");
     for src in &sources {
         let name = src.get_name();
-        let url = src.get_url_address().unwrap_or_else(|| "<unknown>".to_string());
+        let url = src
+            .get_url_address()
+            .unwrap_or_else(|| "<unknown>".to_string());
         let ip = src.get_ip_address();
         match ip {
             Some(ip) => println!("  {} - {} ({})", name, url, ip),
